068b6f5a819a6feda7d1b1de5915abea1550d382
PankratovEP <81170265+PankratovEP@users.noreply.github.com>
2021-05-17 13:59:48 +0300

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 int main()
 {
-   cout << "Enter A and B: ";
-int a, b;
+    cout << "Enter A and B: ";
+double a, b, max, min;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n';
 cout << "A - B = " << a - b << '\n';
-cout  << "A * B = " << a * b << '\n';
-return 0;
+cout << "A * B = " << a * b << '\n';
+cout << "A / B = " << a / b << '\n';
+if(A>B) max=A; else max=B; cout<<max;
+if(A<B) min=A; else min=B; cout<<min;
+    return 0;
 }
